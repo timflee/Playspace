541593c6792d8dcf36f2df1efa3bfdb7315df034
timflee <timflee@hotmail.com>
2024-05-18 21:01:25 +1000

    Looks good in emulation, next to try with real tile

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-"""
-Iterate over each digit in the binary number
-"""
+# A 2D array defines the leds to turn on/off with a letter for the colour to use.
+# 
+# This is then automagically passed and the zip tile colours set accordingly
 i = 0
 j = 0
-
+tileDisplay = Kitronik_Zip_Tile.create_zip_tile_display(1, 1, Kitronik_Zip_Tile.UBitLocations.HIDDEN)
 # Binary number as a string
-ledMap = ["11111", "11001", "10101", "10011", "11111"]
-# Get the length of the binary number
-
-# len(ledMap)
-
+ledMap = ["111r1", "1r001", "10101", "10011", "11111"]
+test = ledMap.index("111r1")
+colourOrder = ["r", "b", "g", "1", "0"]
+colours = [Kitronik_Zip_Tile.colors(ZipLedColors.RED),
+    Kitronik_Zip_Tile.colors(ZipLedColors.BLUE),
+    Kitronik_Zip_Tile.colors(ZipLedColors.GREEN)]
 numRows = len(ledMap)
 numColumns = len(ledMap[0])
-
 while i <= numRows - 1:
-    while j <=numColumns - 1:
-        # Check if the digit is '1'
-        if ledMap[i][j] == "1":
+    while j <= numColumns - 1:
+        colourIndex = colourOrder.index(ledMap[i][j])
+        # if ledMap[i][j] == "1":
+        if colourIndex == 0:
             led.plot(j, i)
+            tileDisplay.set_matrix_color(0, 0, colours[colourIndex])
         j += 1
     j = 0
-    i +=1
+    i += 1
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,24 +1,45 @@
-/** Iterate over each digit in the binary number */
+// A 2D array defines the leds to turn on/off with a letter for the colour to use.
+// 
+// This is then automagically passed and the zip tile colours set accordingly
 let i = 0
 let j = 0
-//  Binary number as a string
-let ledMap = ["11111", "11001", "10101", "10011", "11111"]
-//  Get the length of the binary number
-//  len(ledMap)
+let colourIndex: number;
+let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
+tileDisplay.clear()
+// Binary number as a string
+let ledMap = [
+"111r1",
+"1r001",
+"10101",
+"10011",
+"11111"
+]
+let test = _py.py_array_index(ledMap, "111r1")
+let colourOrder = [
+"r",
+"b",
+"g",
+"1",
+"0"
+]
+let colours = [Kitronik_Zip_Tile.colors(ZipLedColors.Red), Kitronik_Zip_Tile.colors(ZipLedColors.Blue), Kitronik_Zip_Tile.colors(ZipLedColors.Green)]
 let numRows = ledMap.length
 let numColumns = ledMap[0].length
 while (i <= numRows - 1) {
     while (j <= numColumns - 1) {
-        //  Check if the digit is '1'
-        if (ledMap[i][j] == "1") {
+        colourIndex = _py.py_array_index(colourOrder, ledMap[i][j])
+// if ledMap[i][j] == "1":
+        if (colourIndex == 0) {
             led.plot(j, i)
+            tileDisplay.setMatrixColor(0, 0, colours[colourIndex])
         }
-        
         j += 1
     }
     j = 0
     i += 1
 }
-basic.forever(function on_forever() {
-    
+tileDisplay.show()
+tileDisplay.setBrightness(50)
+basic.forever(function () {
+	
 })
