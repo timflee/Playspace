bcfc174771d2339cdc91e816ffb35e962a9e2103
timflee <timflee@hotmail.com>
2024-05-18 21:11:22 +1000

    added more colours

--- main.py
+++ main.py
@@ -4,25 +4,50 @@
 i = 0
 j = 0
 tileDisplay = Kitronik_Zip_Tile.create_zip_tile_display(1, 1, Kitronik_Zip_Tile.UBitLocations.HIDDEN)
+tileDisplay.clear()
 # Binary number as a string
-ledMap = ["111r1", "1r001", "10101", "10011", "11111"]
-test = ledMap.index("111r1")
-colourOrder = ["r", "b", "g", "1", "0"]
-colours = [Kitronik_Zip_Tile.colors(ZipLedColors.RED),
+ledMap = ["11r1rrr",
+    "1r0rrr01",
+    "10101rrr",
+    "100rrr11",
+    "1rrr1111",
+    "111rrrr1",
+    "1r0biv01",
+    "1010gbo1",
+    "100ooo11",
+    "o111y11o"]
+#test = ledMap.index("111r1")
+
+colourOrder = ["k", "b", "g", "i", "o", "p", "r", "v", "w", "y", "0", "1"]
+
+colours = [Kitronik_Zip_Tile.colors(ZipLedColors.BLACK),
     Kitronik_Zip_Tile.colors(ZipLedColors.BLUE),
-    Kitronik_Zip_Tile.colors(ZipLedColors.GREEN)]
+    Kitronik_Zip_Tile.colors(ZipLedColors.GREEN),
+    Kitronik_Zip_Tile.colors(ZipLedColors.INDIGO),
+    Kitronik_Zip_Tile.colors(ZipLedColors.ORANGE),
+    Kitronik_Zip_Tile.colors(ZipLedColors.PURPLE),
+    Kitronik_Zip_Tile.colors(ZipLedColors.RED),
+    Kitronik_Zip_Tile.colors(ZipLedColors.VIOLET),
+    Kitronik_Zip_Tile.colors(ZipLedColors.WHITE),
+    Kitronik_Zip_Tile.colors(ZipLedColors.YELLOW),
+    Kitronik_Zip_Tile.colors(ZipLedColors.BLACK),
+    Kitronik_Zip_Tile.colors(ZipLedColors.WHITE),
+    ]
+    
 numRows = len(ledMap)
 numColumns = len(ledMap[0])
 while i <= numRows - 1:
     while j <= numColumns - 1:
         colourIndex = colourOrder.index(ledMap[i][j])
         # if ledMap[i][j] == "1":
-        if colourIndex == 0:
+        if colourIndex == colourOrder.index("r"):
             led.plot(j, i)
             tileDisplay.set_matrix_color(0, 0, colours[colourIndex])
         j += 1
     j = 0
     i += 1
+tileDisplay.show()
+tileDisplay.set_brightness(50)
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,38 +1,27 @@
-// A 2D array defines the leds to turn on/off with a letter for the colour to use.
-// 
-// This is then automagically passed and the zip tile colours set accordingly
+let colourIndex: number;
+//  A 2D array defines the leds to turn on/off with a letter for the colour to use.
+//  
+//  This is then automagically passed and the zip tile colours set accordingly
 let i = 0
 let j = 0
-let colourIndex: number;
 let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
 tileDisplay.clear()
-// Binary number as a string
-let ledMap = [
-"111r1",
-"1r001",
-"10101",
-"10011",
-"11111"
-]
-let test = _py.py_array_index(ledMap, "111r1")
-let colourOrder = [
-"r",
-"b",
-"g",
-"1",
-"0"
-]
-let colours = [Kitronik_Zip_Tile.colors(ZipLedColors.Red), Kitronik_Zip_Tile.colors(ZipLedColors.Blue), Kitronik_Zip_Tile.colors(ZipLedColors.Green)]
+//  Binary number as a string
+let ledMap = ["11r1rrr", "1r0rrr01", "10101rrr", "100rrr11", "1rrr1111", "111rrrr1", "1r0biv01", "1010gbo1", "100ooo11", "o111y11o"]
+// test = ledMap.index("111r1")
+let colourOrder = ["k", "b", "g", "i", "o", "p", "r", "v", "w", "y", "0", "1"]
+let colours = [Kitronik_Zip_Tile.colors(ZipLedColors.Black), Kitronik_Zip_Tile.colors(ZipLedColors.Blue), Kitronik_Zip_Tile.colors(ZipLedColors.Green), Kitronik_Zip_Tile.colors(ZipLedColors.Indigo), Kitronik_Zip_Tile.colors(ZipLedColors.Orange), Kitronik_Zip_Tile.colors(ZipLedColors.Purple), Kitronik_Zip_Tile.colors(ZipLedColors.Red), Kitronik_Zip_Tile.colors(ZipLedColors.Violet), Kitronik_Zip_Tile.colors(ZipLedColors.White), Kitronik_Zip_Tile.colors(ZipLedColors.Yellow), Kitronik_Zip_Tile.colors(ZipLedColors.Black), Kitronik_Zip_Tile.colors(ZipLedColors.White)]
 let numRows = ledMap.length
 let numColumns = ledMap[0].length
 while (i <= numRows - 1) {
     while (j <= numColumns - 1) {
         colourIndex = _py.py_array_index(colourOrder, ledMap[i][j])
-// if ledMap[i][j] == "1":
-        if (colourIndex == 0) {
+        //  if ledMap[i][j] == "1":
+        if (colourIndex == _py.py_array_index(colourOrder, "r")) {
             led.plot(j, i)
             tileDisplay.setMatrixColor(0, 0, colours[colourIndex])
         }
+        
         j += 1
     }
     j = 0
@@ -40,6 +29,6 @@ while (i <= numRows - 1) {
 }
 tileDisplay.show()
 tileDisplay.setBrightness(50)
-basic.forever(function () {
-	
+basic.forever(function on_forever() {
+    
 })
