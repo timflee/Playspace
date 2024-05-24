243e7fb95f0d51da147410a1a32e888b1e2b6367
timflee <timflee@hotmail.com>
2024-05-24 21:18:06 +1000

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-j = 0
 # A 2D array defines the leds to turn on/off with a letter for the colour to use.
 # 
 # This is then automagically passed and the zip tile colours set accordingly
 i = 0
+j = 0
 tileDisplay = Kitronik_Zip_Tile.create_zip_tile_display(1, 1, Kitronik_Zip_Tile.UBitLocations.HIDDEN)
 tileDisplay.clear()
 tileDisplay.show()
-#control.wait_micros(1000000)
+# control.wait_micros(1000000)
 # Binary number as a string
-ledMapZay = [
-    "bppppppp",
+ledMapZay = ["bppppppp",
     "pppppppp",
     "pppppppp",
     "pbbppppp",
@@ -17,9 +16,7 @@ ledMapZay = [
     "pppppppp",
     "bppppppb",
     "bppppppp"]
-
-ledMapEve1 = [
-    "pppppppp",
+ledMapEve1 = ["pppppppp",
     "piiiiiip",
     "pibbbbip",
     "pibggbip",
@@ -27,9 +24,7 @@ ledMapEve1 = [
     "pibbbbip",
     "piiiiiip",
     "pppppppp"]
-
-ledMap = [
-    "roygbvpw",
+ledMapEve2 = ["roygbvpw",
     "roygbvpw",
     "roygbvpw",
     "roygbvpw",
@@ -37,7 +32,14 @@ ledMap = [
     "roygbvpw",
     "roygbvpw",
     "roygbvpw"]
-
+ledMap = ["b00r0000",
+    "0b0r0000",
+    "00br0000",
+    "000i0000",
+    "wwwpbwww",
+    "000r0b00",
+    "yyyoyygy",
+    "000r000b"]
 ledMapSky = ["vvvvvvvv",
     "vrrrrrrv",
     "vrvrvvrv",
--- main.ts
+++ main.ts
@@ -1,17 +1,18 @@
 let colourIndex: number;
-let j = 0
 //  A 2D array defines the leds to turn on/off with a letter for the colour to use.
 //  
 //  This is then automagically passed and the zip tile colours set accordingly
 let i = 0
+let j = 0
 let tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
 tileDisplay.clear()
 tileDisplay.show()
-// control.wait_micros(1000000)
+//  control.wait_micros(1000000)
 //  Binary number as a string
 let ledMapZay = ["bppppppp", "pppppppp", "pppppppp", "pbbppppp", "pppbpppp", "pppppppp", "bppppppb", "bppppppp"]
 let ledMapEve1 = ["pppppppp", "piiiiiip", "pibbbbip", "pibggbip", "pibggbip", "pibbbbip", "piiiiiip", "pppppppp"]
-let ledMap = ["roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw"]
+let ledMapEve2 = ["roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw", "roygbvpw"]
+let ledMap = ["b00r0000", "0b0r0000", "00br0000", "000i0000", "wwwpbwww", "000r0b00", "yyyoyygy", "000r000b"]
 let ledMapSky = ["vvvvvvvv", "vrrrrrrv", "vrvrvvrv", "vrvvvvrv", "vrvvvvrv", "vvrvvrvv", "vvvrrvvv", "vvvvvvvv"]
 let colourOrder = ["r", "b", "g", "w", "0", "i", "o", "p", "v", "y"]
 let colours = [Kitronik_Zip_Tile.colors(ZipLedColors.Red), Kitronik_Zip_Tile.colors(ZipLedColors.Blue), Kitronik_Zip_Tile.colors(ZipLedColors.Green), Kitronik_Zip_Tile.colors(ZipLedColors.White), Kitronik_Zip_Tile.colors(ZipLedColors.Black), Kitronik_Zip_Tile.colors(ZipLedColors.Indigo), Kitronik_Zip_Tile.colors(ZipLedColors.Orange), Kitronik_Zip_Tile.colors(ZipLedColors.Purple), Kitronik_Zip_Tile.colors(ZipLedColors.Violet), Kitronik_Zip_Tile.colors(ZipLedColors.Yellow)]
